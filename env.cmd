da68c1054ddf59d43020bcc3f02664505494063f
mazharm <mazharm@hotmail.com>
2023-01-20 07:08:55 -0800

    Add files via upload

---
+++ env.cmd
@@ -0,0 +1,8 @@
+set OPENAI_API_KEY=<sign up at openai.com to get your key>
+set OPENAI_PRIVATE_MODEL=<go to openai.com to read up on how to create a fine tune model. Once you have a model, set this env variable to the name of the fine tune model>
+set AZURE_CLIENT_ID=<sign up at portal.azure.com to get your id>
+set AZURE_TENANT_ID=<sign up at portal.azure.com and get your tenant id>
+set AZURE_CLIENT_SECRET=<get your client creds from portal.azure.com>
+set AZURE_STORAGE_ACCOUNT_URL=<create a storage account at portal.azure.com and put the URL here>
+set AZURE_STORAGE_CONTAINER_NAME=<get the storage container name from portal.azure.com>
+set AZURE_STORAGE_ACCOUNT_NAME=<get the storage account name from portal.azure.com>
